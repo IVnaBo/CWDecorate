054961dca80ec1d4f6a6e9f62fa0d6ee8ff601f9
IvnaBo <1361817133@qq.com>
2020-05-10 12:23:11 +0800

    链式库

---
+++ CWDecorate.framework/Headers/CWDecorate.h
@@ -0,0 +1,19 @@
+//
+//  CWDecorate.h
+//  CWDecorate
+//
+//  Created by ivna.evecom on 2020/5/7.
+//  Copyright © 2020 ivna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CWDecorate.
+FOUNDATION_EXPORT double CWDecorateVersionNumber;
+
+//! Project version string for CWDecorate.
+FOUNDATION_EXPORT const unsigned char CWDecorateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CWDecorate/PublicHeader.h>
+
+
